e47515c18914290da6a03f1fac71881ce2bdfa02
luckyluk07 <L.Nojman11@wp.pl>
2024-04-05 20:00:59 +0200

    Add company shares controller

---
+++ ExpenseGuardBackend/Controllers/CompanyShareController.cs
@@ -0,0 +1,69 @@
+﻿using ExpenseGuardBackend.DTOs.CompanyShares;
+using ExpenseGuardBackend.DTOs.Finances;
+using ExpenseGuardBackend.Services.CompanyShares;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseGuardBackend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CompanyShareController : ControllerBase
+	{
+		private readonly ICompanyShareService _companyShareService;
+
+		public CompanyShareController(ICompanyShareService companyShareService)
+		{
+			_companyShareService = companyShareService;
+		}
+
+		[HttpGet]
+		public ActionResult<CompanySharesDto> GetAll()
+		{
+			return Ok(new CompanySharesDto(_companyShareService.GetAll()));
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<CompanyShareDto> Get(int id)
+		{
+			var comapnyShare = _companyShareService.Get(id);
+			if (comapnyShare is null)
+			{
+				return NotFound();
+			}
+			return Ok(comapnyShare);
+		}
+
+		[HttpPost]
+		public ActionResult<CompanyShareDto> Create(CreateCompanyShareDto createCompanyShareDto)
+		{
+			var newCompanyShare = _companyShareService.Create(createCompanyShareDto);
+			if (newCompanyShare is null)
+			{
+				return BadRequest();
+			}
+			return Created($"{Url.Action(nameof(Create))}/{newCompanyShare.Id}", newCompanyShare);
+		}
+
+		[HttpPut("{id}")]
+		public ActionResult<CompanyShareDto> Update(UpdateCompanyShareDto updateCompanyShareDto, int id)
+		{
+			var updatedCompanyShare = _companyShareService.Update(updateCompanyShareDto, id);
+			if (updatedCompanyShare is null)
+			{
+				return NotFound();
+			}
+			return Ok(updatedCompanyShare);
+		}
+
+		[HttpDelete("{id}")]
+		public ActionResult<bool> Remove(int id)
+		{
+			var isCompanyShareRemoved = _companyShareService.Remove(id);
+			if (!isCompanyShareRemoved)
+			{
+				return NotFound();
+			}
+			return Ok(isCompanyShareRemoved);
+		}
+	}
+}
